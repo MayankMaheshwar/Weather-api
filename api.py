5922d1b0ca683bc53724051b8ecec3adfb60facf
MayankMaheshwar <mayank.maheshwari625@gmail.com>
2022-01-07 16:01:37 +0530

    authenticated

---
+++ api.py
@@ -0,0 +1,44 @@
+# import module sys to get the type of exception
+import sys
+
+# needed to make web requests
+import requests
+
+# convert the response as a strcuctured json
+import json
+
+# input
+geo_values = input(
+    "\nEnter the latitude longitude value sepearted by single comma : ")
+
+# making url
+url = "https://api.weather.gov/points/"+geo_values
+print("Error occure Please try again later...")
+
+# getting data
+try:
+
+    # requesting url
+    res = requests.get(url)
+
+    # make json object
+    data = json.loads(res.text)
+    properties = data['properties']
+
+    # getting forecast url
+    forecast = properties['forecast']
+
+    # getting forecast data
+    forecast_res = requests.get(forecast)
+    forecast_data = json.loads(forecast_res.text)
+    forecast_properties = forecast_data['properties']
+    forecast_periods = forecast_properties['periods']
+
+    # finding wednesday night temperature
+    for period in forecast_periods:
+        if period['number'] == 13:
+            print(period['temperature'])
+            break
+
+except:
+    print("Oops!", sys.exc_info()[0], "occurred.")
